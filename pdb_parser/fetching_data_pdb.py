b001b78863455f6b79b1967dc5cadafe93ed3462
Michał Jensko <michaljensko@gmail.com>
2022-07-20 10:08:36 +0200

    Addition of code responsible for fetching data from pdb database
    - listing pdb codes based on input file
    - fetching pdb files from pdb database based on listed codes
    - reporting errors: wrong length of pdb code and failing to find entry in pdb database

---
+++ pdb_parser/fetching_data_pdb.py
@@ -0,0 +1,39 @@
+import requests
+import pathlib
+
+
+def listing_codes(list_of_codes, paths):
+    pdb_codes = []
+    errors = pathlib.Path(__file__).parent
+    errors = errors.joinpath(errors, paths['register'], 'register.txt')
+
+    with open(list_of_codes, 'r') as listing:
+
+        for line in listing:
+            line = line.strip('\n')
+
+            if len(line) > 4:
+                with errors.open(mode="x") as err:
+                    err.write(f'{line}: exceeds standard length for pdb code')
+                continue
+            else:
+                pdb_codes.append(line)
+
+    return pdb_codes
+
+
+def get_pdb_files(pdb_codes, paths):
+    errors = pathlib.Path(__file__).parent
+    errors = errors.joinpath(errors, paths['register'], 'register.txt')
+    pdb_files = pathlib.Path(__file__).parent
+    pdb_files = pdb_files.joinpath(paths['pdb_files'])
+
+    for code in pdb_codes:
+        request = requests.get(f'https://files.rcsb.org/download/{code}.pdb')
+
+        if request.status_code == 200:
+
+            open(pdb_files.joinpath(f'{code}.pdb'), 'wb').write(request.content)
+        else:
+            with errors.open(mode="a") as err:
+                err.write(f'{code}: could not be fetched from pdb database')
